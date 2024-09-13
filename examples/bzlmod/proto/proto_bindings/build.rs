78580ea041d38f2c35fd87a61cb8e86ca7aeae6d
Arjun Panday <arjun@tape.audio>
2024-09-14 01:15:24 +0200

    fix cargo build

--- examples/bzlmod/proto/proto_bindings/build.rs
+++ examples/bzlmod/proto/proto_bindings/build.rs
@@ -1,6 +1,13 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let mut proto_compile_config = prost_build::Config::new();
+
+    // Adds From/Into for chrono (https://lib.rs/crates/prost-wkt-build)
+    proto_compile_config.extern_path(".google.protobuf.Timestamp", "::prost_wkt_types::Timestamp");
+    proto_compile_config.extern_path(".google.protobuf.Duration", "::prost_wkt_types::Duration");
+    proto_compile_config.extern_path(".google.protobuf.Any", "::prost_wkt_types::Any");
+
     tonic_build::configure()
-        .compile(&["proto/helloworld.proto"], &["proto"])
+        .compile_with_config(proto_compile_config, &["proto/helloworld.proto"], &["proto"])
         .expect("Failed to compile proto specification");
     Ok(())
 }
